5cf31d283fd54bf1421f06dee4b5cdb2c1547b07
Nick Jenson <njenson@instructure.com>
2022-02-28 17:22:20 -0800

    add Article and Card components

---
+++ src/lib/components/Articles.svelte
@@ -0,0 +1,18 @@
+<script>
+  import Card from './Card.svelte'
+  export let posts;
+</script>
+
+<div>
+  {#each posts as post}
+    <Card name='edit-3' title={post.meta.title} desc={post.meta.description} href={post.path} />
+  {/each}
+</div>
+
+<style>
+  div {
+    width: max-content;
+    display:flex;
+    justify-content: space-between;
+  }
+</style>
---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,47 @@
+<script>
+  import Icon from './Icon.svelte'
+
+  export let name;
+  export let title;
+  export let desc;
+  export let href;
+</script>
+
+<div>
+  <h4>
+    <span class='icon'>
+      <Icon {name} stroke='#181818' />
+    </span>
+    {title}
+  </h4>
+  <p>{desc}</p>
+  <a {href}>Read more</a>
+</div>
+
+<style>
+  div {
+    padding: 1rem;
+    border: 1px solid var(--border);
+    border-radius: var(--radius);
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  p {
+    font-size: .875rem;
+  }
+
+  h4 {
+    margin: 0;
+    font-size: 1.125;
+  }
+
+  .icon {
+    padding: .4rem 0;
+    border-radius: var(--radius);
+    background-color: var(--text);
+  }
+</style>
