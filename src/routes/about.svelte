187eaab2a8f737e932da848b4b376096df87ff6a
Nick Jenson <nickjenson@gmail.com>
2022-04-24 19:19:19 -0700

    center image

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,21 +1,24 @@
-<img src="/images/memoji.png" alt="Nick Memoji" />
-
-<h3>Hi, I’m Nick!</h3>
-<p>I lead a team of consultants at Instructure where we provide clients and developers expertise on technical aspects of implementation for Canvas LMS. This includes SIS data integrations, branding customization, and authentication troubleshooting.</p>
-
-<p>I like building tools, solving problems and learning new things.
-I started learning software development through writing Ruby scripts and command-line tools to make HTTP requests or perform a task.</p>
-
-<p>I later built an Electron app designed to simplify SIS data conversions and make them more accessible to non-technical teams. It was later built directly into an internal data platform, not bad…right?</p>
-
-<p>I also built and maintain a reactive browser extension in <a href="https://svelte.dev">Svelte</a> that performs tasks across multiple web-apps to help automate and simplify technical tasks. It turns out a lot of time can be with the right APIs.</p>
-
-<p>In my spare time I read about, build, and mess around with software.</p>
-
-<h3>tl;dr</h3>
-<p>I enjoy the thrill of solving difficult or unfamiliar problems, learning new things, or simplifying tasks. And also food.</p>
+<section>
+	<div>
+		<img id="memoji" src="/images/memoji.png" alt="Nick's Memoji" />
+	</div>
+	<h1>Hi, I'm Nick!</h1>
+	<p>I lead a team of consultants at Instructure where we provide clients and developers expertise on technical aspects of implementation for Canvas LMS. This includes SIS data integrations, branding customization, and authentication troubleshooting.</p>
+	<p>I like building tools, solving problems and learning new things.
+	I started learning software development through writing Ruby scripts and command-line tools to make HTTP requests or perform a task.</p>
+	<p>I later built an Electron app designed to simplify SIS data conversions and make them more accessible to non-technical teams. It was later built directly into an internal data platform, not bad…right?</p>
+	<p>I also built and maintain a reactive browser extension in <a href="https://svelte.dev">Svelte</a> that performs tasks across multiple web-apps to help automate and simplify technical tasks. It turns out a lot of time can be with the right APIs.</p>
+	<p>In my spare time I read about, build, and mess around with software.</p>
+	<h3>tl;dr</h3>
+	<p>I enjoy the thrill of solving difficult or unfamiliar problems, learning new things, or simplifying tasks. And also food.</p>
+</section>
 
 <style>
+	section div {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 	img {
 		height: 10rem;
 		border-radius: 50%;
