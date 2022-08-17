c0b80d9f7475f4c81ffb652f8059047a09c3621e
Nick Jenson <nickjenson@gmail.com>
2022-08-17 16:20:49 -0700

    simplify footer component

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -3,24 +3,14 @@
 </script>
 
 <footer>
-	<div class="footer">
-		<div>
-			<Link button="true" href="https://github.com/nickjenson" icon="github" />
-			<Link button="true" href="/rss.xml" icon="rss" />
-		</div>
-		Copyright &copy; 2022 Nick Jenson. All rights reserved.
+	<div class="social">
+		<Link button="true" href="https://github.com/nickjenson" icon="github" />
+		<Link button="true" href="/rss.xml" icon="rss" />
 	</div>
+	Copyright &copy; 2022 Nick Jenson. All rights reserved.
 </footer>
 
 <style>
-	footer > div.footer {
-		margin: 0 auto;
-		max-width: var(--max-width);
-	}
-	footer > div.footer > div {
-		display: flex;
-		justify-content: end;
-	}
 	footer {
 		margin-top: 2rem;
 		width: 100%;
@@ -28,5 +18,9 @@
 		border-top: 1px solid var(--border);
 		background: var(--background-secondary);
 		color: #a8a8a8;
+	}
+	footer .social {
+		display: flex;
+		justify-content: end;
 	}
 </style>
