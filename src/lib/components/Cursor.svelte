4599947cb4d8b5905c77bca8e7474a0f97784663
Nick Jenson <nickjenson@gmail.com>
2022-09-07 17:44:39 -0700

    adjust cursor border radius and blink rate

--- src/lib/components/Cursor.svelte
+++ src/lib/components/Cursor.svelte
@@ -4,15 +4,14 @@
   span {
     margin: 0 .5rem;
     padding: 0 .4rem;
-    border-radius: 2px;
+    border-radius: 1px;
     position: relative;
     bottom: 2px;
     vertical-align: baseline;
     background-color: var(--text);
-    font-size: 1.4rem;
-    animation: 1.5s blink step-end infinite;
+    font-size: 1.3rem;
+    animation: 2s blink step-end infinite;
   }
-
   @keyframes blink {
     from, to {
       background-color: transparent;
