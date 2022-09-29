f1586a573c9ae6a388b3db585f5ebc19acd59ad1
Nick Jenson <nickjenson@gmail.com>
2022-09-29 15:08:37 -0700

    update initial transitions

--- src/lib/components/Cursor.svelte
+++ src/lib/components/Cursor.svelte
@@ -1,40 +1,58 @@
 <script>
 	export let text;	
-	let offset = 3;
+	export let offset = 5;
+	export let timeout = 250;
+	
 	let index = 0;
-	let typing = true;
+	let blink = false;
 	
 	let first = text.slice(0, text.length - offset);
 	const last = text.slice(-offset);
 	
 	function type() {
-		if (index <= offset) { 
+		if (index < offset) { 
 			char = last[index];
 			first += char;
 			index++;
+			setTimeout(type, timeout);
+		} else {
+			blink = true
 		}
 	}
 	
-	$: char = index == offset ? " " : last[index];
-	const typed = () => setTimeout(type, 800);
-	typed();
+	$: char = index == offset ? "" : last[index];
+	type();
+
+	
+
 </script>
 
 <h1>
 	{first}
-	<span>{char}</span>
+	<span class:blink >{@html char}</span>
 </h1>
+
+
+
+
 
 <style>
 	h1 {
 		margin: 0;
+		font-size: 1.8rem;
+		font-family: var(--font-mono);
 	}
 	span {
 		border-radius: 1px;
 		position: absolute;
 		vertical-align: baseline;
-		color: var(--dark-text);
-		background-color: var(--dark-background);
+		color: var(--background);
+		background-color: var(--text);
+	}
+	span.blink {
+		height: 2rem;
+		margin: .2rem .5rem;
+		padding: 0 0.45rem;
 		animation: 1.6s blink step-end infinite;
 	}
 	@keyframes blink {
